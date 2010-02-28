948a85ecf20488843fd6119c0f7a7f73b23216fd
Doug Rogers <dsrogersjr@gmail.com>
2010-02-28 10:43:44 -0600

    added login system
    Signed-off-by: Doug Rogers <dsrogersjr@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 ActionController::Routing::Routes.draw do |map|
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login '/login', :controller => 'public', :action => 'index'
+  map.register '/register', :controller => 'users', :action => 'create'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.resources :users
+
+  map.resource :session
+  map.resource :publics
+
+  map.resources :questions, :has_one => :surveys
+  map.resources :surveys, :has_many => :questions
+  
+  map.connect ':controller/:action/:id'
+  map.connect ':controller/:action/:id.:format'
+  map.root :controller => "public", :action => "index"
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -38,6 +54,5 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+
 end
